1bd84646f7ccfb7e5216dd59ca8eaf5ee08c48c0
neisser <neisser2909@gmail.com>
2024-07-06 18:28:34 -0400

    Implementado interceptor global en el proyecto

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,40 +1,36 @@
 import { Body, Controller, Get, Param, Post, Put, Delete, HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
 import { TaskDTO } from './dto/task.dto';
 import { TaskService } from './task.service';
-import { resolve } from 'path';
-import { rejects } from 'assert';
 
 @Controller('api/v1/task')
 export class TaskController {
-    constructor(private readonly TaskService: TaskService) {}
+    constructor(private readonly taskService: TaskService) {}
 
     @Post()
     create(@Body() TaskDTO:TaskDTO){
-        //throw new BadRequestException('Error en Peticion');
-        //throw new HttpException('Error en Peticion', HttpStatus.BAD_REQUEST);
-        return new Promise((resolve, reject) => {
-            setTimeout(() => reject('Error en Peticion'), 2000);
-        });
-        //return this.TaskService.create(TaskDTO);
+        /*return new Promise((resolve, reject) => {
+            setTimeout(() => reject('Something was wrong!'), 5000);
+        })*/
+        return this.taskService.create(TaskDTO);
     }
 
     @Get()
     findAll(){
-        return this.TaskService.findAll();
+        return this.taskService.findAll();
     }
 
     @Get(':id')
     findById(@Param('id') id:string) {
-        return this.TaskService.findById(id);
+        return this.taskService.findById(id);
     }
 
     @Put(':id')
     update(@Param('id') id:string, @Body() taskDTO:TaskDTO) {
-        return this.TaskService.update(id, taskDTO);
+        return this.taskService.update(id, taskDTO);
     }
 
     @Delete(':id')
     delete(@Param('id') id:string) {
-        return this.TaskService.delete(id);
+        return this.taskService.delete(id);
     }
 }
