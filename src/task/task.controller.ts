c3abe318b74e7e42456db05c94c7d3219579a695
neisser <neisser2909@gmail.com>
2024-07-06 16:43:29 -0400

    Creado el crud de la aplicacion

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { TaskDTO } from './dto/task.dto';
+import { TaskService } from './task.service';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly TaskService: TaskService) {}
+
+    @Post()
+    create(@Body() TaskDTO:TaskDTO){
+        return this.TaskService.create(TaskDTO);
+    }
+
+    @Get()
+    findAll(){
+        return this.TaskService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id:string) {
+        return this.TaskService.findById(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id:string, @Body() taskDTO:TaskDTO) {
+        return this.TaskService.update(id, taskDTO);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string) {
+        return this.TaskService.delete(id);
+    }
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { ITask } from '../task/task.interface';
+import { TaskDTO } from './dto/task.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    tasks: ITask[] = [];
+
+    create(taskDTO: TaskDTO): ITask {
+        const task = {
+            id: uuidv4(),
+            ...taskDTO,
+        };
+        this.tasks.push(task);
+        return task;
+    };
+
+    findAll(): ITask[] {
+        return this.tasks;
+    }
+
+    findById(id:string): ITask {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    update(id:string, taskDTO:TaskDTO): ITask {
+        const newTask = {id, ...taskDTO}
+        this.tasks = this.tasks.map(task => task.id === id ? newTask: task);
+        return newTask;
+    }
+
+    delete(id:string): string {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+        return 'Task deleted'
+    }
+}
