a1ae67de25d0d1f1eae57767695d382e84aee0eb
neisser <neisser2909@gmail.com>
2024-07-06 18:15:51 -0400

    Implementada Excepcion global en la aplicacion

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete, HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
 import { TaskDTO } from './dto/task.dto';
 import { TaskService } from './task.service';
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 @Controller('api/v1/task')
 export class TaskController {
@@ -8,7 +10,12 @@ export class TaskController {
 
     @Post()
     create(@Body() TaskDTO:TaskDTO){
-        return this.TaskService.create(TaskDTO);
+        //throw new BadRequestException('Error en Peticion');
+        //throw new HttpException('Error en Peticion', HttpStatus.BAD_REQUEST);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject('Error en Peticion'), 2000);
+        });
+        //return this.TaskService.create(TaskDTO);
     }
 
     @Get()
